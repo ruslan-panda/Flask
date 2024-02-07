8f5675ef85541bc4d356731ebbb3b84c278921cb
ruslan-panda <rusruss1003@mail.ru>
2024-02-07 16:49:07 +0300

    123

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -29,6 +29,23 @@ def promo():
                         <h5>Присоединяйся!</h5>
                       </body>
                     </html>"""
+
+
+@app.route('/image_mars')
+def mars():
+    return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <title>Привет, Марс!</title>
+                          </head>
+                          <body>
+                            <h1>Жди нас, Марс!</h1>
+                            <img src="{url_for('static', filename='img/mars.png')}" 
+                            alt="здесь должна была быть картинка, но не нашлась">'
+                            <h5>Вот она какая, красная планета</h5>
+                          </body>
+                        </html>"""
 
 
 if __name__ == '__main__':
