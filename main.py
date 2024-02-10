98a8ef57d6b48eeafbf8482fbdf27783323a21d3
ruslan-panda <rusruss1003@mail.ru>
2024-02-10 10:01:30 +0300

    1234

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for
+from requests import request
 
 app = Flask(__name__)
 
@@ -83,6 +84,73 @@ def prom_img():
                             </div>
                           </body>
                         </html>"""
+
+
+@app.route('/form_sample', methods=['POST', 'GET'])
+def form_sample():
+    return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример формы</title>
+                          </head>
+                          <body>
+                            <h1>Форма для регистрации в суперсекретной системе</h1>
+                            <div>
+                                <form class="login_form" method="post">
+                                    <input type="text" class="form-control" id="text" aria-describedby="textHelp" placeholder="Введите фамилию"text">
+                                    <input type="text" class="form-control" id="text" placeholder="Введите имя" name="text">
+                                    <label for="classSelect"></label>
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое у вас образование</label>
+                                        <select class="form-control" id="classSelect" name="class">
+                                          <option>Дошкольное</option>
+                                          <option>Начальное</option>
+                                          <option>Чуть выше начального</option>
+                                          <option>Среднее</option>
+                                          <option>Выше среднего</option>
+                                          <option>Высшее</option>
+                                        </select>
+                                        <label for="classSelect">Какие у Вас профессии</label>
+                                    </div>
+                                    <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="sex" id="female" value="female">
+                                            <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                            </label>
+                                        </div>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-login_form-input" type="checkbox" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
 
 
 if __name__ == '__main__':
