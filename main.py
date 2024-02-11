16da998529f4daad51e852ea7530228429ffa44d
ruslan-panda <rusruss1003@mail.ru>
2024-02-11 11:45:59 +0300

    12345

--- main.py
+++ main.py
@@ -98,10 +98,13 @@ def form_sample():
                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                             crossorigin="anonymous">
                             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Пример формы</title>
+                            <title>Отбор астронавтов</title>
                           </head>
                           <body>
-                            <h1>Форма для регистрации в суперсекретной системе</h1>
+                            <div align="center">
+                                <h2>Анкета Претендента</h2>
+                                <h4>на участие  миссии</h4>
+                            </div>
                             <div>
                                 <form class="login_form" method="post">
                                     <input type="text" class="form-control" id="text" aria-describedby="textHelp" placeholder="Введите фамилию"text">
@@ -122,46 +125,139 @@ def form_sample():
                                         <label for="classSelect">Какие у Вас профессии</label>
                                     </div>
                                     <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" checked>
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Инженер-исследователь
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Пилот
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Строитель
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Экзобиолог
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Врач
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male">
+                                        <label class="form-check-label" for="male">
+                                        Инженер по терраформированию
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Климатолог
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Специалист по радиационной защите
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Астрогеолог, гляциолог
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Инженер жизнеобеспечения
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Метеоролог
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Оператор марсохода
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Киберинженер
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Штурман
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="sex" id="male" value="male" >
+                                        <label class="form-check-label" for="male">
+                                        Пилот дронов
+                                        </label>
+                                    </div> 
+                                <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male">
                                           <label class="form-check-label" for="male">
                                             Мужской
                                           </label>
                                         </div>
                                         <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="sex" id="female" value="female">
-                                            <label class="form-check-label" for="female">
-                                            Женский
-                                          </label>
-                                        </div>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="sex" id="female" value="female">
-                                          <label class="form-check-label" for="female">
-                                            Женский
-                                            </label>
-                                        </div>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-login_form-input" type="checkbox" name="sex" id="female" value="female">
+                                          <input class="form-check-input" type="radio" name="sex" id="female">
                                           <label class="form-check-label" for="female">
                                             Женский
                                           </label>
                                         </div>
-                                </form>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="lam">
+                                          <label class="form-check-label" for="female">
+                                            Ламинат
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="par">
+                                          <label class="form-check-label" for="female">
+                                            Паркет
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готов остаться на Марсе?</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Записаться</button>
                             </div>
                           </body>
                         </html>'''
 
-
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
-    return render_template('index.html', title=title)
-
-
-@app.route('/training/<prof>')
-def trainind(prof):
-    return render_template('treining.html', title="Тренировки в полёте", prof=prof)
 
 
 if __name__ == '__main__':
